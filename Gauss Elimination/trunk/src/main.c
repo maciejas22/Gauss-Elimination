1f979ce7023b4b08ae466274c3697baca3adc2c3
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2021-12-21 22:45:20 +0100

    Update main.c

--- Gauss Elimination/trunk/src/main.c
+++ Gauss Elimination/trunk/src/main.c
@@ -13,20 +13,23 @@ int main(int argc, char ** argv) {
 
 	if (A == NULL) return -1;
 	if (b == NULL) return -2;
-	printToScreen(A);
-	printToScreen(b);
 
 	res = eliminate(A,b);
-	x = createMatrix(b->r, 1);
-	if (x != NULL) {
-		res = backsubst(x,A,b);
+    if(res == 1) {
+        fprintf(stderr,"Błąd! Dzielenie przez 0.\n");
+        return 1;
+    }
 
-		printToScreen(x);
-	  freeMatrix(x);
-	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
-	}
-
+	x = createMatrix(b->rows, 1);
+    res = backsubst(x,A,b);
+    if(res == 2) {
+        fprintf(stderr,"Błąd! Macierz nieprawidlowych rozmiarow.\n");
+        return 2;
+    }
+    printToScreen(A);
+    printToScreen(b);
+    printToScreen(x);
+    freeMatrix(x);
 	freeMatrix(A);
 	freeMatrix(b);
 
