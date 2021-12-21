7c02d60276d645675e34a541ebd79e5f81f1d979
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2021-12-21 20:29:02 +0100

    Update backsubst.c

--- Gauss Elimination/trunk/src/backsubst.c
+++ Gauss Elimination/trunk/src/backsubst.c
@@ -1,22 +1,40 @@
 #include "backsubst.h"
+#include <stdio.h>
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+    //jezeli macierz nie jest kwadratowa return 2
+    if(mat->columns != mat->rows) {
+        return 2;
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    //dla latwosci obliczen zostanie utworzona nowa macierz *tmp, ktora bedzie polaczeniem macierzy *mat i *b
+    int i, j;
+    Matrix *tmp = createMatrix(mat->rows, mat->columns+1);
+    for(i = 0; i < mat->rows; i++) {
+        for(j = 0; j < mat->columns; j++) {
+            tmp->data[i][j] = mat->data[i][j];
+        }
+        tmp->data[i][mat->columns] = b->data[i][0];
+    }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    //funckja odpowiadajaca za podstawienie wsteczne
+    double s;
+    (*x->data[mat->rows-1]) = tmp->data[mat->rows-1][mat->rows] / tmp->data[mat->rows-1][mat->rows-1];
+    for(i=mat->rows-2; i>=0; i--)
+    {
+        s=0;
+        for(j=i+1; j<mat->rows; j++)
+        {
+            s += (tmp->data[i][j]*(*x->data[j]));
+            *x->data[i] = (tmp->data[i][mat->rows]-s)/tmp->data[i][i];
+        }
+    }
 
-				return 0;
+    return 0;
 }
 
 
